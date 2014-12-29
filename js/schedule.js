268dff17e354011d0ac58368bf1639052bb31d8b
Andy Morgan <ajm87@bath.ac.uk>
2014-12-29 18:38:55 +0000

    Added drag & drop schedule

---
+++ js/schedule.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	var item;
+    var order;
+    $( "#sortable" ).sortable({
+        revert: true,
+        beforeStop: function(event, ui) {
+        	item = ui.item;
+        },
+        receive: function() {
+        	switch($(item).text()) {
+        		case 'Pomodoro':
+        			$(item).attr("id", "pomodoro");
+        			break;
+        		case 'Short Break':
+        			$(item).attr("id", "short_break");
+        			break;
+        		case 'Long Break':
+        			$(item).attr("id", "long_break");
+        			break;
+        	}
+        },
+        update: function() {
+            order = $('#sortable').sortable('toArray'); 
+            $('#timerlist').text(order);
+        }
+    }).droppable({greedy:true});
+    
+    $( ".draggable" ).draggable({
+        connectToSortable: "#sortable",
+        helper: "clone",
+        revert: "invalid"
+    });
+    $('body').droppable({
+        drop: function(event, ui) {
+            ui.draggable.remove();
+        }
+    });
+    $( "ul, li" ).disableSelection();
+
+    $('#begin').click(function() {
+
+    });
+
+});
