14591f9b6c1553d1a3d544c5dcf7b9828ffe288e
Jarrod Birch <jarrod@jarrodbirch.co.uk>
2015-01-27 16:04:09 +0000

    Revert "Delete unnecessary files"
    This reverts commit 861f1cd5d8fe4835032d88ca86a3dfc90eef8363.

---
+++ js/schedule.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	var item;
+
+    $( "#sortable" ).sortable({
+        revert: true,
+        beforeStop: function(event, ui) {
+        	item = ui.item;
+        },
+        receive: function() {
+        	switch($(item).text()) {
+        		case 'Pomodoro':
+        			$(item).attr("id", "pomodoro");
+        			break;
+        		case 'Short Break':
+        			$(item).attr("id", "short_break");
+        			break;
+        		case 'Long Break':
+        			$(item).attr("id", "long_break");
+        			break; 
+        	}
+        },
+        update: function() {
+            order = $('#sortable').sortable('toArray'); 
+            $('#timerlist').text(order);
+            updateUpcoming(false);
+        }
+    }).droppable({greedy:true});
+    
+    $( ".draggable" ).draggable({
+        connectToSortable: "#sortable",
+        helper: "clone",
+        revert: "invalid"
+    });
+    $('body').droppable({
+        drop: function(event, ui) {
+            if(ui.draggable.attr("id") != "no_delete") {
+                ui.draggable.remove();
+            }
+            updateUpcoming(false);
+        }
+    });
+    $( "ul, li" ).disableSelection();
+});
